4fc6846b4b1a466c073740d05b2545df71beede0
hungnguyen <hoanghungmt@gmail.com>
2025-09-05 13:31:59 -0700

    Update 4.3_create_merchant_lookup_table.sql

--- scripts/azure_databricks_notebook/4.3_create_merchant_lookup_table.sql
+++ scripts/azure_databricks_notebook/4.3_create_merchant_lookup_table.sql
@@ -1,7 +1,7 @@
 -- =====================================================================================
 -- File: 4.3_create_merchant_lookup_table.sql
 -- Description: Create and populate merchant lookup table in Silver layer
--- Created: 2025-01-31
+-- Created: 2025-07-31
 -- =====================================================================================
 
 -- ## Overview
